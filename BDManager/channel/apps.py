8c1f512711a90d4d2a8a823e67a0a55f77cc8ef7
CalicoBox <calicobox@foxmail.com>
2017-07-14 16:33:14 +0800

    '完成与hive的交互'

--- BDManager/channel/apps.py
+++ BDManager/channel/apps.py
@@ -112,6 +112,7 @@ def GetHiveTableDesc(request):
     cur=conn.cursor()
     cur.execute('desc '+tableName)
     tableDesc = cur.fetchall()
+    print(tableDesc)
     cur.close()
     conn.close()
     res['success'] = True
