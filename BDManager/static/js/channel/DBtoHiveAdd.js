86ff50b5cd50b5ab416a28f63497ae17c32715b4
CalicoBox <calicobox@foxmail.com>
2017-07-14 16:31:37 +0800

    '完成与hive的交互'

--- BDManager/static/js/channel/DBtoHiveAdd.js
+++ BDManager/static/js/channel/DBtoHiveAdd.js
@@ -623,6 +623,7 @@ require(['jquery','jquery.bootstrap','jquery.datetimepicker','common','quickSear
                 success: function (result) {
                     showloading(false);
                     if (result && result.success) {
+                        alert(result.hiveTableDesc);
                         _this.setHiveTableElm(result.hiveTableDesc);
                     } else {
                         $.showModal({content: "查询失败"+result.err});
