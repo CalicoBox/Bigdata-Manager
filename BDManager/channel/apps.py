66e5b2ac89eb1fc31bf8686d0a069e4373d04195
CalicoBox <calicobox@foxmail.com>
2017-07-18 15:23:28 +0800

    完成DB导入hive功能

--- BDManager/channel/apps.py
+++ BDManager/channel/apps.py
@@ -125,6 +125,25 @@ def GetHiveTableDescription(request):
     res['err'] = err
     return JsonResponse(res)
 
+class switch(object):
+  def __init__(self, value):
+    self.value = value
+    self.fall = False
+  def __iter__(self):
+    """Return the match method once, then stop"""
+    yield self.match
+    raise StopIteration
+  def match(self, *args):
+    """Indicate whether or not to enter a case suite"""
+    if self.fall or not args:
+      return True
+    elif self.value in args: # changed for v1.5, see below
+      self.fall = True
+      return True
+    else:
+      return False
+
+
 def DBtoHive(request):
   # conMode = request.GET['conMode']
   conMode = 'normal'
